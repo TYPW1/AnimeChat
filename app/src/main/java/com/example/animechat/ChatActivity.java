b7349674811d820e39302dd4a904f0741f3d5881
022052914D <Tiyapiwi1999>
2023-05-07 14:01:20 +0200

    Implement character-specific chat functionality with ChatGPT API integration

--- app/src/main/java/com/example/animechat/ChatActivity.java
+++ app/src/main/java/com/example/animechat/ChatActivity.java
@@ -1,27 +1,98 @@
 package com.example.animechat;
 
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
     private RecyclerView chatRecyclerView;
     private EditText chatInput;
     private Button sendButton;
+    private List<Message> chatMessages;
+
+    private ChatGPTService chatGPTService;
+
+    private Character character;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        chatGPTService = new ChatGPTService();
+
+        character = (Character) getIntent().getSerializableExtra("character");
+        chatGPTService.setCharacter(character.getName());
+
+
         chatRecyclerView = findViewById(R.id.chat_recycler_view);
         chatInput = findViewById(R.id.chat_input);
         sendButton = findViewById(R.id.send_button);
 
-        // Implement your chat functionality with GPT API
+        chatMessages = new ArrayList<>();
+
+        final ChatAdapter chatAdapter = new ChatAdapter(chatMessages);
+        chatRecyclerView.setAdapter(chatAdapter);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = chatInput.getText().toString();
+                if (!messageText.isEmpty()) {
+                    sendMessage(messageText);
+                }
+            }
+        });
     }
+
+    private void sendMessage(final String messageText) {
+        chatMessages.add(new Message(messageText, true));
+        chatInput.setText("");
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                String response = chatGPTService.getChatResponse(messageText);
+                if (response != null) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray choices = jsonResponse.getJSONArray("choices");
+                        JSONObject choice = choices.getJSONObject(0);
+                        JSONObject messageObj = choice.getJSONObject("message");
+                        String reply = messageObj.getString("content");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                chatMessages.add(new Message(reply, false));
+                                chatRecyclerView.getAdapter().notifyDataSetChanged();
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            chatMessages.add(new Message("Error: Could not get a response from the API.", false));
+                            chatRecyclerView.getAdapter().notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/animechat/ChatGPTService.java
+++ app/src/main/java/com/example/animechat/ChatGPTService.java
@@ -1,5 +1,9 @@
 package com.example.animechat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -9,36 +13,78 @@ import okhttp3.Response;
 
 public class ChatGPTService {
     private static final String API_KEY = "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";
-    private static final String API_URL = "https://api.openai.com/v1/engines/davinci-codex/chat/completions";
+    private static final String API_URL = "https://api.openai.com/v1/chat/completions";
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private OkHttpClient client;
+    private String characterName;
+
+    public void setCharacter(String characterName) {
+        this.characterName = characterName;
+    }
+
 
     public ChatGPTService() {
         client = new OkHttpClient();
     }
 
-    public String getChatResponse(String prompt) {
-        String json = "{ \"prompt\": \"" + prompt + "\", \"max_tokens\": 100, \"temperature\": 0.7, \"n\": 1 }";
-        RequestBody body = RequestBody.create(json, JSON);
+    public String getChatResponse(String message) {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
 
+
+
+        JSONArray messages = new JSONArray();
+
+        if (characterName != null) {
+            JSONObject systemMessage = new JSONObject();
+            try {
+                systemMessage.put("role", "system");
+                systemMessage.put("content", "You are " + characterName + ". " +
+                                                        "Act exactly like the character. " +
+                                                        "use all expressions exactly as the character.");
+                messages.put(systemMessage);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        JSONObject userMessage = new JSONObject();
+        try {
+            userMessage.put("role", "user");
+            userMessage.put("content", message);
+            messages.put(userMessage);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("model", "gpt-3.5-turbo");
+            requestBody.put("messages", messages);
+            requestBody.put("temperature", 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(JSON, requestBody.toString());
         Request request = new Request.Builder()
                 .url(API_URL)
-                .post(body)
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Authorization", "Bearer " + API_KEY)
+                .post(body)
                 .build();
 
         try {
             Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                return response.body().string();
-            } else {
+            if (!response.isSuccessful()) {
                 throw new IOException("Unexpected code " + response);
             }
+            return response.body().string();
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
+        return null;
     }
+
 }
